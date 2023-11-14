b87dbdd79d76087633b6538516af549ea31b88e5
Rikard Olsson <znittzel@gmail.com>
2023-11-14 11:47:50 +0100

    Changed such that there's no need for PropKey, just propositions

--- README.md
+++ README.md
@@ -75,14 +75,16 @@ Then use by sending a POST request to (default) `http://localhost:8080/evaluate`
             "key": "def"
         }
     ],
-    "interpretation": {
-        "a": 0,
-        "b": 1,
-        "x": 1
-    }
+    "interpretations": [
+        {
+            "a": 0,
+            "b": 1,
+            "x": 1
+        }
+    ]
 }
 ```
 If the first proposition is true, given the interpretation, then `abc` will be returned. If the second proposition is true, given the interpretation, then `def` will be returned. If neither is true, then an empty list will be returned.
 
 ## No restriction of complexity
-Because of the data structure of a proposition, you could construct it with no restriction of complexity. For instance, one proposition could contain the full combi9nation space of an advanced product system with billions and billions of combinations, or the proposition could be a simple `Variable` proposition with only two combinations. The API will be able to evaluate it fast and efficiently in both cases.
+Because of the data structure of a proposition, you could construct it with no restriction of complexity. For instance, one proposition could contain the full combination space of an advanced product system with billions and billions of combinations, or the proposition could be a simple `Variable` proposition with only two combinations. The API will be able to evaluate it fast and efficiently in both cases.
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use actix_web::{post, web, App, HttpResponse, HttpServer};
-use puan_pv::PropKey;
+use actix_web::{get, post, web, App, HttpResponse, HttpServer};
+use puan_pv::{Proposition, PropT};
 use serde::{Serialize, Deserialize};
 use std::collections::HashMap;
 
@@ -8,30 +8,42 @@ pub type StringInterpretation = HashMap<String, f64>;
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct EvaluableInterpretation {
-    evaluables: Vec<PropKey>,
-    interpretation: StringInterpretation,
+    propositions: Vec<Proposition>,
+    interpretations: Vec<StringInterpretation>,
 }
 
 #[post("evaluate")]
-pub async fn evaluate(evaluable_interpretation: web::Json<EvaluableInterpretation>) -> HttpResponse {
-    let evaluated: Vec<String> = evaluable_interpretation.0
-        .evaluables
-        .iter()
-        .filter_map(
-            |x| 
-            x.evaluate(&evaluable_interpretation.0.interpretation)
-        ).collect();
+pub async fn evaluate(proposition_interpretations: web::Json<EvaluableInterpretation>) -> HttpResponse {
     HttpResponse::Ok()
         .content_type(APPLICATION_JSON)
         .json(
-            evaluated
+            proposition_interpretations.0
+                .propositions
+                .iter()
+                .map(
+                    |proposition| 
+                    proposition_interpretations.0.interpretations.iter().enumerate().filter_map(
+                        |(index, interpretation)| 
+                        match proposition.interpret(interpretation) == 1.0 {
+                            true => Some(index),
+                            false => None,
+                        }
+                    ).collect::<Vec<usize>>()
+                ).collect::<Vec<Vec<usize>>>(),
         )
+}
+
+#[get("/health")]
+async fn health_check() -> HttpResponse {
+    HttpResponse::Ok().body("OK")
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
-        App::new().service(evaluate)
+        App::new()
+            .service(evaluate)
+            .service(health_check)
     })
     .bind(("127.0.0.1", 8080))?
     .run()
